6f7a47cf85f24e25aba073f184829d0b6af87877
Sheray Franklin <sherayfranklin@gmail.com>
2021-05-18 01:59:53 -0400

    smooth scrolling

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+    $(".aboutMeButton").hover(function(){
+      $(this).css("background-color", "#F2CBBD");
+      }, function(){
+      $(this).css("background-color", "#F2DAD1");
+    });
+});
+
+$(document).ready(function(){
+    $("button").hover(function(){
+      $(this).css("background-color", "#F2CBBD");
+      }, function(){
+      $(this).css("background-color", "#F2DAD1");
+    });
+});
+
+$(document).ready(function() {
+    var scrollLink = $('.nav-linkscroll');
+
+    // Smooth Scrolling
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top 
+        }, 1000)
+    })
+
+
+
+})
